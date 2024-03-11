8118162d966f49ca9e9e1cdef66a9a4293ad558e
faustoFF <kulakov89@gmail.com>
2024-03-11 16:53:37 +0300

    auto set up exception handler

--- src/Exceptions/Reportable.php
+++ src/Exceptions/Reportable.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\Log;
 
 class Reportable
 {
-    public function __invoke(): callable
+    public function __invoke(): ?callable
     {
-        return function (\Throwable $e) {
-            if (config('contextify.enabled') && config('contextify.notifications.enabled')) {
+        return config('contextify.enabled') && config('contextify.notifications.enabled')
+            ? function (\Throwable $e) {
                 try {
                     app(config('contextify.notifications.notifiable'))
                         ->notify(new ExceptionOccurredNotification($e))
@@ -19,6 +19,6 @@ class Reportable
                     Log::error("Unable to send exception occurred notification: {$e}");
                 }
             }
-        };
+            : null;
     }
 }
